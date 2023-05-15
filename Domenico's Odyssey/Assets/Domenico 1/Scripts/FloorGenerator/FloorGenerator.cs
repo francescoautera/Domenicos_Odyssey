2beeaa359c9dfa1832d3c25940436151c7b3eb74
francescoautera <francesco.autera@gmail.com>
2023-05-15 16:38:38 +0200

    add namespace

--- Domenico's Odyssey/Assets/Domenico 1/Scripts/FloorGenerator/FloorGenerator.cs
+++ Domenico's Odyssey/Assets/Domenico 1/Scripts/FloorGenerator/FloorGenerator.cs
@@ -3,30 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloorGenerator : MonoBehaviour {
+namespace Domenico1 {
 
 	
-	[SerializeField] private GameObject floorObject;
-	private Transform parentTransform;
+	public class FloorGenerator : MonoBehaviour {
 
-	public Transform ParentTransform {
-		get => parentTransform;
-		set => parentTransform = value;
-	}
 
-	public void SpawnFloor(Transform positionPrevFloor,Transform parent) {
-		var vectorOffset = new Vector3(0, positionPrevFloor.localScale.y * 10, 0);
-		var floor = Instantiate(floorObject, positionPrevFloor.position - vectorOffset , Quaternion.identity, parent);
-		var floorGeneratorComponent = floor.GetComponent<FloorGenerator>();
-		floorGeneratorComponent.ParentTransform = parent;
-	}
-	
-	private void OnTriggerEnter2D(Collider2D col) {
-		if (col.gameObject.CompareTag("Player")) {
-			SpawnFloor(transform,parentTransform);
+		[SerializeField] private GameObject floorObject;
+		private Transform parentTransform;
+
+		public Transform ParentTransform {
+			get => parentTransform;
+			set => parentTransform = value;
+		}
+
+		public void SpawnFloor(Transform positionPrevFloor, Transform parent) {
+			var vectorOffset = new Vector3(0, positionPrevFloor.localScale.y * 10, 0);
+			var floor = Instantiate(floorObject, positionPrevFloor.position - vectorOffset, Quaternion.identity, parent);
+			var floorGeneratorComponent = floor.GetComponent<FloorGenerator>();
+			floorGeneratorComponent.ParentTransform = parent;
+		}
+
+		private void OnTriggerEnter2D(Collider2D col) {
+			if (col.gameObject.CompareTag("Player")) {
+				SpawnFloor(transform, parentTransform);
+			}
 		}
 	}
-}
 
+}
 
 
