471bf3ffb916fe7b8d05e2d3792cdb75954d58e9
francescoautera <francesco.autera@gmail.com>
2023-05-15 17:33:12 +0200

    minor changes

--- Domenico's Odyssey/Assets/Domenico 1/Scripts/CharacterController.cs
+++ Domenico's Odyssey/Assets/Domenico 1/Scripts/CharacterController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public Action<GameObject> OnPlayerDeath;
+    public Action OnPlayerDeath;
     
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.CompareTag("Obstacle"))
             {
-                OnPlayerDeath?.Invoke(gameObject);
+                OnPlayerDeath?.Invoke();
             }
         }
 }
--- Domenico's Odyssey/Assets/Domenico 1/Scripts/FloorGenerator/ObstacleGenerator.cs
+++ Domenico's Odyssey/Assets/Domenico 1/Scripts/FloorGenerator/ObstacleGenerator.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class ObstacleGenerator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+namespace Domenico1 {
+
+    public class ObstacleGenerator : MonoBehaviour {
+
+        public GameObject obstacle;
+        public Vector2 randomTimerSpawn;
+        public Vector2 randomOffsetSpawn;
+        private GameObject lastObject;
+        private float currentTimer;
+        private float timerToInstance;
+        private void Awake() {
+            timerToInstance = Random.Range(randomTimerSpawn.x, randomTimerSpawn.y);
+        }
+
+        private void Update() {
+            currentTimer += Time.deltaTime;
+            if (currentTimer > timerToInstance) {
+                currentTimer = 0;
+                Spawn(lastObject !=null ? lastObject.transform : transform);
+                timerToInstance = Random.Range(randomTimerSpawn.x, randomTimerSpawn.y);
+            }
+        }
+
+
+        private void Spawn(Transform pos) {
+            var xOffset = Random.Range(-randomOffsetSpawn.x, randomOffsetSpawn.x);
+            var yOffset = Random.Range(-randomOffsetSpawn.y, -randomOffsetSpawn.y * 2);
+            Vector3 posToInstance = new Vector3(pos.position.x + xOffset,pos.position.y+yOffset);
+            lastObject =  Instantiate(obstacle, posToInstance, Quaternion.identity);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
