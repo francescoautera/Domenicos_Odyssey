49e49ed9d9aef49eaf05c298591e7cfcda15c5a4
francescoautera <francesco.autera@gmail.com>
2023-05-15 20:18:32 +0200

    add hand tutorial

--- Domenico's Odyssey/Assets/Domenico 1/Scripts/SliderController.cs
+++ Domenico's Odyssey/Assets/Domenico 1/Scripts/SliderController.cs
@@ -10,21 +10,30 @@ namespace Domenico1 {
         private CharacterController characterController;
         [SerializeField] private Slider slider;
         [SerializeField] private Vector2 minMaxViewportRange;
+        [SerializeField] private GameObject handTutorial;
         private Camera cameraMain;
+        bool isFirstTime;
+        
         // Start is called before the first frame update
         void Start() {
             
             cameraMain = Camera.main;
             characterController = FindObjectOfType<CharacterController>();
+            handTutorial.SetActive(true);
+            isFirstTime = true;
         }
 
         // Update is called once per frame
         void Update() {
-            Debug.Log(slider.value);
+
+            if (slider.value != 0.5f && isFirstTime) {
+                handTutorial.SetActive(false);
+                isFirstTime = false;
+            }
+            
             float xPos = Mathf.Clamp(slider.value,minMaxViewportRange.x,minMaxViewportRange.y);
             Vector3 pos = cameraMain.ViewportToWorldPoint(new Vector3(xPos, characterController.transform.position.y,characterController.transform.position.z));
-            Debug.Log(pos.x);
-           
+
             characterController.SetPos(pos.x);
         }
     }
